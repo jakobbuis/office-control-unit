8049d0ee264e85c7224b2405053dfd3bb7c59220
Jakob Buis <jakob@jakobbuis.nl>
2017-11-18 22:42:46 +0100

    add status indicator for sonos message

--- src/components/PublicAddress.vue
+++ src/components/PublicAddress.vue
@@ -1,23 +1,31 @@
 <template>
     <div class="box">
         <h2>Sonos public address system</h2>
-        <form action="#" @submit.prevent="address">
-            <div class="field has-addons">
-                <p class="control">
-                    <span class="select">
-                        <select v-model="selectedZone">
-                            <option v-for="zone in zones">{{ zone }}</option>
-                        </select>
-                    </span>
-                </p>
-                <p class="control is-expanded">
-                    <input class="input" type="text" placeholder="Message" v-model="message">
-                </p>
-                <p class="control">
-                    <button type="submit" class="button is-primary">Send</button>
-                </p>
+        <div class="content">
+            <form action="#" @submit.prevent="address">
+                <div class="field has-addons">
+                    <p class="control">
+                        <span class="select">
+                            <select v-model="selectedZone">
+                                <option v-for="zone in zones">{{ zone }}</option>
+                            </select>
+                        </span>
+                    </p>
+                    <p class="control is-expanded">
+                        <input class="input" type="text" placeholder="Message" v-model="message" @keyup="clearStatus">
+                    </p>
+                    <p class="control">
+                        <button type="submit" class="button is-primary">Send</button>
+                    </p>
+                </div>
+            </form>
+            <div class="status" v-if="status != null">
+                <div>
+                    <i class="fa fa-fw" :class="statusClass"></i>
+                    {{ statusMessage }}
+                </div>
             </div>
-        </form>
+        </div>
     </div>
 </template>
 
@@ -31,7 +39,35 @@ export default {
             zones: [],
             selectedZone: null,
             message: '',
+            status: null,
+            lastError: null,
         };
+    },
+
+    computed: {
+        statusMessage() {
+            if (this.status === 'sending') {
+                return 'Playing message';
+            }
+            if (this.status === 'success') {
+                return 'Message delivered';
+            }
+            if (this.status === 'error') {
+                return `Error: ${this.lastError}`;
+            }
+        },
+
+        statusClass() {
+            if (this.status === 'sending') {
+                return 'fa-circle-o-notch fa-spin';
+            }
+            if (this.status === 'success') {
+                return 'fa-check';
+            }
+            if (this.status === 'error') {
+                return 'fa-times';
+            }
+        },
     },
 
     created() {
@@ -45,16 +81,39 @@ export default {
 
     methods: {
         address() {
+            this.status = 'sending';
             const zone = encodeURIComponent(this.selectedZone);
             const message = encodeURIComponent(`Mededeling: ${this.message}. Herhaling: ${this.message}`);
-            axios.get(`http://klimaat:5005/${zone}/say/${message}/nl`);
+            axios.get(`http://klimaat:5005/${zone}/say/${message}/nl`).then((response) => {
+                this.status = response.data.status;
+            }).catch((error) => {
+                if (error.response) {
+                    this.status = error.response.data.status;
+                    this.lastError = error.response.data.error;
+                }
+                else {
+                    this.status = 'error';
+                    this.lastError = 'unknown error occurred';
+                }
+            });
+        },
+
+        clearStatus() {
+            this.status = null;
+            this.lastError = null;
         }
     },
 };
 </script>
 
-<style scoped>
-form {
+<style lang="scss" scoped>
+.content {
     padding: 1em;
+}
+.status {
+    font-style: italic;
+
+    .fa-check { color: green; }
+    .fa-times { color: red; }
 }
 </style>
