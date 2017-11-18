dd1a19cde99ae94ca521560bd7776a6773f829cc
Jakob Buis <jakob@jakobbuis.nl>
2017-11-18 21:43:56 +0100

    fleshed out inoperable UI for sonos public address

--- src/components/PublicAddress.vue
+++ src/components/PublicAddress.vue
@@ -1,5 +1,38 @@
 <template>
     <div class="box">
         <h2>Sonos public address system</h2>
+        <form action="#">
+            <div class="field has-addons">
+                <p class="control">
+                    <span class="select">
+                        <select>
+                            <option>Activate</option>
+                            <option>Developers</option>
+                            <option>Producers</option>
+                        </select>
+                    </span>
+                </p>
+                <p class="control is-expanded">
+                    <input class="input" type="text" placeholder="Message">
+                </p>
+                <p class="control">
+                    <a class="button is-primary">
+                        Send
+                    </a>
+                </p>
+            </div>
+        </form>
     </div>
 </template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+form {
+    padding: 1em;
+}
+</style>
